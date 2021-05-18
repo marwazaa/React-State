dc89b024153da280309bf11fca3528670c97aee3
unknown <marwa.zaabi2012@gmail.com>
2021-05-18 15:43:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import image from "./img.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+state={isVisible:false}
+
+person=[
+  'marwa zaabi',
+  'student at Gomycode',
+  'student',
+]
+
+showV=()=>{
+  this.setState({
+    isVisible:!this.state.isVisible,
+  
+  })
+  
 }
+
+render(){
+  return (
+  <div>
+    
+   <center>
+      {this.state.isVisible ? (
+         <>  <button onClick={this.showV}> Info </button>
+          {this.person.map((person)=>(
+      
+             <h1>{person}</h1> 
+             ) )
+           }
+
+       <center>
+        <img src={image} style={{width:300,heigth:200, marginBottom:30}}/>
+       </center>
+     
+        </>
+      )
+          :<button onClick={this.showV}> Show info </button> 
+       
+      }
+
+    </center>
+   </div>
+
+  )
+  
+ 
+  
+}
+}
+
 
 export default App;
